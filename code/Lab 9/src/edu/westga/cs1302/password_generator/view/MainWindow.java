ab618b4cac831cb135463c239690b7197521c935
Markees-Ewers <Me00070@my.westga.edu>
2024-11-21 02:36:57 -0500

    added file menu to close and about
    made the file so that it could be added and closed

---
+++ code/Lab 9/src/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -0,0 +1,63 @@
+package edu.westga.cs1302.password_generator.view;
+
+import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/** Codebehind for the MainWindow of the Application.
+ * 
+ * @author CS 1302
+ * @version Fall 2024
+ */
+public class MainWindow {
+
+    @FXML private CheckBox mustIncludeDigits;
+    @FXML private CheckBox mustIncludeLowerCaseLetters;
+    @FXML private CheckBox mustIncludeUpperCaseLetters;
+    @FXML private TextField minimumLength;
+    @FXML private TextArea output;
+    @FXML private Label errorTextLabel;
+    @FXML private Button generatePasswordButton;
+    @FXML private MenuItem closeMenuItem;
+    @FXML private MenuItem aboutMenuItem;
+    
+    private ViewModel vm;
+    
+    @FXML
+    void initialize() {
+    	this.vm = new ViewModel();
+    	this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
+    	this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
+    	this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
+    	this.minimumLength.setText(this.vm.getMinimumLength().getValue());
+    	this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
+    
+    	this.output.textProperty().bind(this.vm.getPassword());
+    	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
+    	
+    	this.generatePasswordButton.setOnAction(
+    			(event) -> { 
+    				this.vm.generatePassword();
+    			} 
+    	);
+    	this.closeMenuItem.setOnAction(event -> {
+    		Stage stage = (Stage) (this.output.getScene().getWindow());
+    		stage.close();
+    	});
+    	
+    	this.aboutMenuItem.setOnAction(event -> {
+    		Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setContentText("Created By Markees Ewers for school password generator creates random passwords for you to use");
+    		alert.setHeaderText("About Password Generator");
+    		alert.showAndWait();
+    	});
+    }
+}
